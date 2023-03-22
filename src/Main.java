bea620b354d62cd79c9f89b05cf2fc7b317710f3
amina <amina@aminas-MacBook.local>
2023-03-22 23:41:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        RectangleArea rectangle = new RectangleArea();
+        rectangle.getData();
+        rectangle.fieldDisplay();
+    }
+}
